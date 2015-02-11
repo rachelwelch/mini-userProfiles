66f94440e5d4f97e2f1d548e3e56448d5918b1ed
Rachel Welch <rachelmsmeltzer@gmail.com>
2015-02-10 19:59:56 -0700

    update service to use an AJAX request

--- controller.js
+++ controller.js
@@ -2,10 +2,14 @@ var app = angular.module('userProfiles');
 
 app.controller('MainController', function($scope, mainService) {
     
-    $scope.getData = function() {
-      $scope.users = mainService.getUsers();  
-    };
+    var getUsers = function() {
+      mainService.getUsers().then(function(data){
+        $scope.users = data;
+        console.log($scope.users);
+      });
+    }
     
-    $scope.getData();
-    console.log($scope.users);
+    getUsers();
+    
+    
 });
